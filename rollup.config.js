22e36a0b1e82b384dc19f199e9f63fe406f71e58
hanool <29415530+hanool@users.noreply.github.com>
2021-12-20 21:38:19 +0900

    🔧 Edit rollup.config to use export default

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,11 @@
-import typescript from '@rollup/plugin-typescript'
+import typescript from "@rollup/plugin-typescript";
 
 export default {
-  input: 'src/index.ts',
+  input: "src/index.ts",
   output: {
-    dir: 'lib',
-    format: 'cjs'
+    dir: "lib",
+    format: "cjs",
+    exports: "default",
   },
-  plugins: [typescript()]
+  plugins: [typescript()],
 };
